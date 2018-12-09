3302ecd84302f7e30d9b7e0240d79655d1f9113e
taith <tai.thanhtran21@gmail.com>
2018-12-10 00:55:30 +0100

    correction

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -16,7 +16,7 @@ public class PaletteRasterImage implements Image{
         this.width = width;
         this.height = height;
         this.palettes = new ArrayList<color>;
-        this.indexeofcolors = new int [width][height];
+        this.indexesOfColors = new int [width][height];
         palettes.add(color);
         for(int i = 0 ; i< height ; i++){
             for(int y = 0 ; y<width ; y++){
@@ -35,6 +35,11 @@ public class PaletteRasterImage implements Image{
                 indexesOfColors[y][i] = palettes.indexOf(pixels[y][i]);
             }
         }
+
+    }
+
+    public void createRepresentation(){
+
 
     }
 
